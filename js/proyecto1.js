b39909d3b804fdd731c242043aecf1e9e1ee7f9b
lferreram <lferreram@yahoo.com>
2022-11-23 14:22:30 -0600

    subiendo primer proyecto

---
+++ js/proyecto1.js
@@ -0,0 +1,23 @@
+const object1 = {a:"1",
+                 b:"2",
+                 c:"3", }
+
+console.log(Object.keys(object1));
+console.log(Object.values(object1));
+
+const object2 = {a:"apple",
+                 b:"microsoft",
+                 c:"google",}
+
+console.log(Object.keys(object2));
+console.log(Object.values(object2));
+
+ 
+const object3 = {key1:"true",
+                 key2:"false",
+                 key3:"undefined",}
+
+console.log(Object.keys(object3));
+console.log(Object.values(object3));                
+
+
